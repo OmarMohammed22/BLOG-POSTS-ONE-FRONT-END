1392e3785ecd7df14850c69db8e76460ee267a37
OmarMohammed22 <106238409+OmarMohammed22@users.noreply.github.com>
2022-06-20 16:25:27 +0200

    adding project

---
+++ STATIC/scripts/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#signin-popup-open").click(function(){
+        $("#signin-popup").css("display", "block");
+    });
+
+    $("#signin-popup-close").click(function(){
+        $("#signin-popup").css("display", "none");
+    });
+
+});
